9a44f58c7832ebcdd1f4164e838df481bc88a590
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-11-30 20:37:52 -0800

    Adds jest to dependencies in index.js and adds constructor consts for employee, managager, engineer, intern.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // Dependencies
 const inquirer = require('inquirer');
 const fs = require("fs");
+const jest = require('jest');
+
+// Constructors 
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
