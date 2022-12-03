338616d7f64a981a7d604cd6c9f5ef70a067de47
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-03 13:31:01 -0800

    Adds comments of things to test in employee test and starts tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,32 @@
 const Employee = require("../lib/Employee");
 
-test("makes an employee object", () => {
-    const employee = new Employee("Luke");
-});
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+  });
 
-test()
+  test("Can set name via constructor arguments", () => {
+    const name = "Alice";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+  });
+
+  
+
+
+
+
+
+// test("makes an employee object", () => {
+//     const employee = new Employee("Luke");
+// });
+
+
+// Can instantiate Employee instance
+// Can set name via constructor arguments
+// Can set id via constructor argument (1ms)
+// Can set email via constructor argument
+// Can get name via getName) (2ms)
+// Can get id via getIdO (2ms)
+// Can get email via getEmail/ (4ms)
+// getRole() should return "Emplovee"
