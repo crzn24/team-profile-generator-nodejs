b524f4457866a724d4d5646e3b9d93e4c179cbc4
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-02 18:48:09 -0800

    adds function to addManager and inquirer prompts

--- index.js
+++ index.js
@@ -16,6 +16,68 @@ const generate = require('./src/html-template')
 
 function initApp() {
 
+    // add a manager when starting up
+    function addManager() {
+        console.log("Let's put together a team!");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter the team manager's name."
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's ID?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid ID for the manager.";
+                }  
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter an email address for the manager.";
+                }  
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter an office number.";
+                }  
+            }
+        ]).then(answers => {
+
+        });
+    }
+
+
+
+
+
+
+
+
+
+
     
 }
 
