0ee7e69b1d06bdd2f5e5d698a856f7488e13626d
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-03 13:47:55 -0800

    Finishes  test for employee?

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,32 +1,61 @@
 const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
-    const e = new Employee();
-    expect(typeof(e)).toBe("object");
-  });
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
 
-  test("Can set name via constructor arguments", () => {
-    const name = "Alice";
-    const e = new Employee(name);
-    expect(e.name).toBe(name);
-  });
+test("Can set name via constructor arguments", () => {
+  const name = "Luke";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
 
-  
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("luke", testValue);
+  expect(e.id).toBe(testValue);
+});
 
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("luke", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
 
+test("Can get name via getName()", () => {
+  const testValue = "Luke";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
 
+test("Can get id via getId()", () => {
+  const testValue = 100;
+  const e = new Employee("Luke", testValue);
+  expect(e.getId()).toBe(testValue);
+});
 
+test("Can get email via getEmail()", () => {
+  const testValue = "luke@test.com";
+  const e = new Employee("Luke", 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
+test('getRole() should return "Employee"', () => {
+  const testValue = "Employee";
+  const e = new Employee("Luke", 1, "luke@test.com");
+  expect(e.getRole()).toBe(testValue);
+});
 
 // test("makes an employee object", () => {
 //     const employee = new Employee("Luke");
 // });
 
-
 // Can instantiate Employee instance
 // Can set name via constructor arguments
-// Can set id via constructor argument (1ms)
+// Can set id via constructor argument
 // Can set email via constructor argument
-// Can get name via getName) (2ms)
-// Can get id via getIdO (2ms)
-// Can get email via getEmail/ (4ms)
+// Can get name via getName
+// Can get id via getIdO
+// Can get email via getEmail
 // getRole() should return "Emplovee"
