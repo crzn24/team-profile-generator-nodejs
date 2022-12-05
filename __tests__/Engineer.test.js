54ee5dab801cbbf9a1c2e847f7f9e7c474607691
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-04 16:26:56 -0800

    Finishes tests for engineer with pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,26 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+test ("Can set GitHub account via constructor", () => {
+    const testValue = "MyGitHub";
+    const e = new Engineer("Luke", 1, "engineer@email.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test ("getRole() should return 'Engineer'", () => {
+    const testValue = 'Engineer';
+    const e = new Engineer("Luke", 1, "engineer@email.com", "MyGitHub");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test ("Can get GitHub username via getGithub()", () => {
+    const testValue = 'GitHubAccount';
+    const e = new Engineer("Luke", 1, "engineer@email.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
 
 
 
-
-// Can set GitHUb account via constructor
+// Can set GitHub account via constructor
 // getRole() should return "Engineer"
 // Can get GitHub username via getGithub()
