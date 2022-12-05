7921546fa6d50525dc9e7e284642b04d96272d7c
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-04 16:36:27 -0800

    Finishes tests for intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,15 +2,21 @@ const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
 
 test("Can set school via constructor", () => {
-    
+    const testValue = "UW";
+    const int = new Intern("Luke", 1, "intern@email.com", testValue);
+    expect(int.school).toBe(testValue);
 });
 
 test("getRole() should return 'Intern'", () => {
-    
+    const testValue = 'Intern';
+    const int = new Intern("Luke", 1, "intern@email.com", "UW");
+    expect(int.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
-    
+    const testValue = "UW";
+    const int = new Intern("Luke", 1, "intern@email.com", testValue);
+    expect(int.getSchool()).toBe(testValue);
 });
 
 
