c741fdc47cc6cee2516fffbefcdd88ab1368f808
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-04 16:29:09 -0800

    Starts tests for intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,6 +1,17 @@
+const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern');
 
+test("Can set school via constructor", () => {
+    
+});
 
+test("getRole() should return 'Intern'", () => {
+    
+});
 
+test("Can get school via getSchool()", () => {
+    
+});
 
 
 // Can set school via constructor
