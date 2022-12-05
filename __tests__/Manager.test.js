e39d2994951cb65475e85e70ed16da420da53a87
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-04 16:11:06 -0800

    Completes manager test and passes

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,23 @@
+const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
 
+test("Can set office number via constructor argument", () => {
+    const testValue = 100;
+    const m = new Manager("Luke", 1, 'manager@email.com', testValue);
+    expect(m.officeNumber).toBe(testValue);
+});
 
+test("getRole() should return 'Manager'", () => {
+    const testValue = 'Manager';
+    const m = new Manager("Luke", 1, 'manager@email.com', 100);
+    expect(m.getRole()).toBe(testValue);
+});
 
-
-
-
+test("Can get office number via getOffice", () => {
+    const testValue = 100;
+    const m = new Manager("Luke", 1, 'manager@email.com', testValue);
+    expect(m.getOfficeNumber()).toBe(testValue);
+});
 
 
 // Can set office number via constructor argument
